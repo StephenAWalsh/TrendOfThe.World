fe434b49757b16136fb5c0e73b7858b01483b7d4
Steve Walsh <steve.walsh@transfueler.com>
2017-10-19 14:17:48 -0700

    Revert "breaking various components into modules"

--- server.js
+++ server.js
@@ -4,22 +4,18 @@ const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = process.env.DATABASE_URL
+const conString = process.env.DATABASE_URL || "postgres://tom:myPassword@localhost:5432/trending"
 
 const client = new pg.Client(conString);
 client.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static('./public/'));
 
-app.listen(PORT, function() {
-  console.log(`Server started on port ${PORT}!`);
-});
 //Google News
 
 app.post('/google-news', function(request, response) {
@@ -265,4 +261,9 @@ app.get('/youtube', function(request, response) {
   .catch(function(err) {
     console.error(err)
   })
+});
+
+
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}!`);
 });
